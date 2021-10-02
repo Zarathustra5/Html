9ed66d35c39d88bfeec584985066678a3b0111ad
Comp <official1908teamfbi@gmail.com>
2021-10-02 23:05:46 +0300

    new file:   durak/img/warrior.png 	new file:   durak/index.html 	new file:   durak/prepros.config 	new file:   durak/source/css/nullstyle.css 	new file:   durak/source/css/style.css 	new file:   durak/source/js/script.js 	new file:   durak/source/prepros.config 	new file:   durak/source/scss/nullstyle.scss 	new file:   durak/source/scss/style.scss 	deleted:    moneyExchange/.index.html.swp 	new file:   moneyExchange/img/bg-el.svg 	deleted:    moneyExchange/img/bg.svg 	new file:   moneyExchange/img/bitcoin.svg 	new file:   moneyExchange/img/bitcoin2.svg 	new file:   moneyExchange/img/etherium.svg 	new file:   moneyExchange/img/etherium2.svg 	new file:   moneyExchange/img/exchange.svg 	new file:   moneyExchange/img/facebook.svg 	new file:   moneyExchange/img/hello-bg1.svg 	new file:   moneyExchange/img/hello-bg2.svg 	new file:   moneyExchange/img/hello-bg_el1.svg 	new file:   moneyExchange/img/hello-bg_el2.svg 	new file:   moneyExchange/img/history.svg 	new file:   moneyExchange/img/instagram.svg 	new file:   moneyExchange/img/paypal.svg 	new file:   moneyExchange/img/paypal2.svg 	new file:   moneyExchange/img/pm.svg 	new file:   moneyExchange/img/pm2.svg 	new file:   moneyExchange/img/privat.svg 	new file:   moneyExchange/img/privat2.svg 	new file:   moneyExchange/img/qiwi.svg 	new file:   moneyExchange/img/qiwi2.svg 	new file:   moneyExchange/img/referals.svg 	modified:   moneyExchange/img/sberbank.svg 	new file:   moneyExchange/img/sberbank2.svg 	new file:   moneyExchange/img/security.svg 	new file:   moneyExchange/img/support.svg 	new file:   moneyExchange/img/vk.svg 	new file:   moneyExchange/img/yandex.svg 	new file:   moneyExchange/img/yandex2.svg 	modified:   moneyExchange/index.html 	modified:   moneyExchange/source/css/style.css 	modified:   moneyExchange/source/js/script.js 	modified:   moneyExchange/source/scss/style.scss 	modified:   template/source/scss/style.scss

--- moneyExchange/source/js/script.js
+++ moneyExchange/source/js/script.js
@@ -1,2 +1,45 @@
 //Строгий режим
 "use strict"
+
+//Кнопка переключения данных с отдаете в получаете--------------------------
+let giveField = document.querySelector('.give__currency');
+let getField = document.querySelector('.get__currency');
+let get = document.querySelector('.get');
+let give = document.querySelector('.give');
+let transport = document.querySelector('.transport');
+transport.onclick = function(){
+  if (transport.classList.contains('_active')){
+    give.append(giveField);
+    get.append(getField);
+    transport.classList.remove('_active');
+  }else{
+    get.append(giveField);
+    give.append(getField);
+    transport.classList.add('_active');
+  }
+}
+
+//Список кошельков----------------------------------------------
+let getChoose = document.querySelector('.get__choose');
+let giveChoose = document.querySelector('.give__choose');
+let getList = document.querySelector('.get__list');
+let giveList = document.querySelector('.give__list');
+getChoose.onclick = function(){
+  if (getChoose.classList.contains('_active')){
+    getList.style.display = "none";
+    getChoose.classList.remove('_active');
+  }else{
+    getList.style.display = "block";
+    getChoose.classList.add('_active');
+    getChoose.style.transform('none');
+  }
+}
+giveChoose.onclick = function(){
+  if (giveChoose.classList.contains('_active')){
+    giveList.style.display = "none";
+    giveChoose.classList.remove('_active');
+  }else{
+    giveList.style.display = "block";
+    giveChoose.classList.add('_active');
+  }
+}
