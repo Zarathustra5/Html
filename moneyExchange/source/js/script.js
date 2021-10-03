a12b8b48ae226b18fcfbdb747afa44bbebf0062a
Comp <official1908teamfbi@gmail.com>
2021-10-03 10:29:37 +0300

    modified:   moneyExchange/index.html 	modified:   moneyExchange/source/css/style.css 	modified:   moneyExchange/source/js/script.js 	modified:   moneyExchange/source/scss/style.scss

--- moneyExchange/source/js/script.js
+++ moneyExchange/source/js/script.js
@@ -2,44 +2,66 @@
 "use strict"
 
 //Кнопка переключения данных с отдаете в получаете--------------------------
-let giveField = document.querySelector('.give__currency');
-let getField = document.querySelector('.get__currency');
+let fieldGive = document.querySelector('.give__currency');
+let fieldGet = document.querySelector('.get__currency');
 let get = document.querySelector('.get');
 let give = document.querySelector('.give');
 let transport = document.querySelector('.transport');
 transport.onclick = function(){
   if (transport.classList.contains('_active')){
-    give.append(giveField);
-    get.append(getField);
-    transport.classList.remove('_active');
+    give.append(fieldGive);
+    get.append(fieldGet);
   }else{
-    get.append(giveField);
-    give.append(getField);
-    transport.classList.add('_active');
+    get.append(fieldGive);
+    give.append(fieldGet);
   }
+  transport.classList.toggle('_active');
 }
 
 //Список кошельков----------------------------------------------
-let getChoose = document.querySelector('.get__choose');
-let giveChoose = document.querySelector('.give__choose');
-let getList = document.querySelector('.get__list');
-let giveList = document.querySelector('.give__list');
-getChoose.onclick = function(){
-  if (getChoose.classList.contains('_active')){
-    getList.style.display = "none";
-    getChoose.classList.remove('_active');
+let chooseGive = document.querySelector('.give__choose');
+let chooseGet = document.querySelector('.get__choose');
+let listGive = document.querySelector('.give__list');
+let listGet = document.querySelector('.get__list');
+let animationGive = document.querySelector('.give__animation');
+let animationGet = document.querySelector('.get__animation');
+chooseGive.onclick = function(){
+  if (chooseGive.classList.contains('_active')){
+    listGive.style.display = "none";
+    animationGive.style.transform = "none";
   }else{
-    getList.style.display = "block";
-    getChoose.classList.add('_active');
-    getChoose.style.transform('none');
+    listGive.style.display = "block";
+    animationGive.style.transform = "rotate(180deg)";
+  }
+  chooseGive.classList.toggle('_active');
+}
+
+chooseGet.onclick = function(){
+  if (chooseGet.classList.contains('_active')){
+    listGet.style.display = "none";
+    animationGet.style.transform = "none";
+    
+  }else{
+    listGet.style.display = "block";
+    animationGet.style.transform = "rotate(180deg)";
+  }
+  chooseGet.classList.toggle('_active');
+}
+
+//Список кошельков----------------------------------------------
+let liGive = document.querySelectorAll('.give__list li');
+let liGet = document.querySelectorAll('.get__list li');
+let valueGet = document.querySelector('.get__value');
+let valueGive = document.querySelector('.give__value');
+for (let li of liGive){
+  li.onclick = function(){
+    valueGive.firstElementChild.setAttribute('src', li.firstElementChild.getAttribute('src'));
+    valueGive.lastElementChild.textContent = li.lastElementChild.textContent;
   }
 }
-giveChoose.onclick = function(){
-  if (giveChoose.classList.contains('_active')){
-    giveList.style.display = "none";
-    giveChoose.classList.remove('_active');
-  }else{
-    giveList.style.display = "block";
-    giveChoose.classList.add('_active');
+for (let li of liGet){
+  li.onclick = function(){
+    valueGet.firstElementChild.setAttribute('src', li.firstElementChild.getAttribute('src'));
+    valueGet.lastElementChild.textContent = li.lastElementChild.textContent;
   }
 }
